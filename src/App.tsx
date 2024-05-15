6c07c156b4d1d59b5ee7e9fd83a247a2ff8b1eae
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-15 17:06:53 +0530

    build a resoonsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import React from "react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <Button colorScheme='blue'>Button</Button>
-  )
-}
+    <Grid templateAreas={{
+      base:`"header " " main"`,
+      lg:`"header header" "aside main"`
+    }}>
+      <GridItem pl="5" bg="red" area={"header"}>
+        header
+      </GridItem>
+      <Show above='lg'>
+      <GridItem pl="5" bg="green" area={"aside"}>
+        aside
+      </GridItem>
+      </Show>
+      <GridItem pl="5" bg="yellow" area={"main"}>
+        main
+      </GridItem>
+    </Grid>
+  );
+};
 
-export default App
+export default App;
